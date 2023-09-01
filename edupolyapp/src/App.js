b38d600bc17a34be675cf47da943cb9dd1304994
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-09-01 19:19:07 +0530

    sdfsdf

---
+++ edupolyapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+
+
+function App() {
+  return (
+    <div className="mybox">
+      <h1>ReactJS</h1>
+
+      
+     
+    </div>
+  );
+}
+
+export default App;
